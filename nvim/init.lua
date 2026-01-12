3e08d8cd6bea22dd865f21d70da573ff69039940
skye <3878135216@qq.com>
2026-01-12 18:48:06 +0800

    themes of (kitty, neovim) -> catppuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,5 +10,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- 使用TokyoNight主题
-vim.cmd("colorscheme tokyonight-moon")
+-- 让 Neovim 背景透明度跟随 Kitty
+vim.cmd("autocmd VimEnter * hi Normal guibg=NONE")
+
+-- 使用catppuccin主题
+ vim.cmd("colorscheme catppuccin-macchiato")
